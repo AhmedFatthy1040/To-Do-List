a10f64c21fbe86a6756ff1378f4fdd396dacc6c4
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2022-06-24 03:52:42 +0200

    first commit

---
+++ ToDoList.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#ifndef ToDoListH
+#define ToDoListH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TEdit *TaskInputField;
+	TButton *AddButton;
+	TListBox *TasksListBox;
+	void __fastcall AddButtonClick(TObject *Sender);
+	void __fastcall TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ __astcache/c@@users@ahmed@onedrive@المستندات@embarcadero@studio@projects/todolist.cpp
@@ -0,0 +1,30 @@
+﻿//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "ToDoList.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::AddButtonClick(TObject *Sender)
+{
+	String task = TaskInputField -> Text;
+	TasksListBox -> Items -> Add(task);
+	TaskInputField -> Text = "" ;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item)
+
+{
+	int index = Item -> Index;
+	TasksListBox -> Items -> Delete(index);
+}
+//---------------------------------------------------------------------------
